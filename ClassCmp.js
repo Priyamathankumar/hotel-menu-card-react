bc7bff5e01b1508fae179356055f4675c18936c5
Priyamathankumar <selvapriyasagi@gmail.com>
2024-07-20 14:35:10 +0530

    Initial commit

---
+++ ClassCmp.js
@@ -0,0 +1,16 @@
+import React from "react"
+class ClassCmp extends React.Component{
+    
+    render(){
+        return(
+            <div className="add"> <hr></hr><h2 >Address</h2>
+             <h3>xxyyzz</h3>
+             <h4 className="ph">  📞Phone   +91 12345 67890</h4>
+
+                
+                </div>
+        )
+        
+    }
+}
+export default ClassCmp;
---
+++ FunctionCmp.js
@@ -0,0 +1,58 @@
+import React from "react";
+function FunctionCmp(){
+    return(
+        <>
+        <div>
+        <body>
+            <div className="box">
+            <h1 className="head">Billal Hotel  🍽️</h1>
+            <ul className="main"><h2 >MAIN FOOD 🥘</h2>
+            <li>Idly   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ₹10</li>
+            <li>Dosai &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  ₹50 </li>
+            <li>Parotta  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;₹15</li>
+            <li>Chicken Briyani &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ₹120</li>
+            <li>Prawn Briyani &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;₹150</li>
+            <li>Veg Meal &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ₹100</li>
+            <li>Sambar Rice &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;₹50</li>
+            <li>Curd Rice &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;₹50</li>
+            <li>Lemone Rice&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ₹50</li></ul>
+            <ul className="side"><h2 > SideDish 🍗</h2>
+            <li>Chicken 65  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  ₹100</li>
+            <li>Prawn 65  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp;  &nbsp;&nbsp;₹150</li>
+            <li>Gobi 65  &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp;&nbsp;₹70</li>
+            <li>Egg Podimas &nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; &nbsp;₹50</li>
+            <li>omlate  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;₹50</li></ul>
+            <ul className="des"><h2 >Desserts</h2>
+              <ul> <h3>Milksakes 🥛</h3>
+              <li>Vennila  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;₹50</li>
+              <li>Butterscoth  &nbsp; &nbsp; &nbsp; &nbsp; ₹50</li>
+              <li>Chocolate  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ₹50</li>
+              <li>Blackcurent  &nbsp; &nbsp; &nbsp; &nbsp; ₹50</li>
+              <li>Strawberry  &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; ₹50</li></ul> 
+              <ul><h3>Icecream 🍨🍦</h3>
+               <li>Vennila  &nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp;&nbsp;₹100</li>
+              <li>Butterscoth &nbsp; &nbsp; &nbsp; ₹100</li>
+              <li>Chocolate &nbsp; &nbsp; &nbsp; &nbsp; ₹100</li>
+              <li>Blackcurent &nbsp; &nbsp; &nbsp;  ₹100</li>
+              <li>Strawberry &nbsp; &nbsp; &nbsp;  &nbsp; ₹100</li>
+           </ul>
+            <ul> <h3>Fresh Juice 🍸</h3>
+            <li>Apple     &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ₹70</li>
+            <li>Orange     &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ₹70</li>
+            <li>Pomegranate  &nbsp; &nbsp; ₹70</li>
+            <li>Watermelaon &nbsp; &nbsp; ₹70</li>
+            </ul>
+            </ul>
+            
+            </div>
+            
+            
+            
+</body>
+
+
+        </div>
+        </>
+    );
+}
+export default FunctionCmp;
